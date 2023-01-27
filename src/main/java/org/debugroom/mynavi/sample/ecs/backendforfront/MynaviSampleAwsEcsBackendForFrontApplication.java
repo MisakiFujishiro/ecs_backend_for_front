d3f4f9c742145e4fec180f52f74e2d8f923c79fb
misaki_fujishiro <Misaki.Fujishiro@nttdata.com>
2023-01-27 15:16:41 +0900

    first commit

---
+++ src/main/java/org/debugroom/mynavi/sample/ecs/backendforfront/MynaviSampleAwsEcsBackendForFrontApplication.java
@@ -0,0 +1,13 @@
+package org.debugroom.mynavi.sample.ecs.backendforfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynaviSampleAwsEcsBackendForFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynaviSampleAwsEcsBackendForFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/debugroom/mynavi/sample/ecs/backendforfront/MynaviSampleAwsEcsBackendForFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package org.debugroom.mynavi.sample.ecs.backendforfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynaviSampleAwsEcsBackendForFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
